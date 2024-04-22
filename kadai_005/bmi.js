cde50262ca6cd8bac79a358817fb1bf123f49b27
hagiotomohiro <164848969+hagiotomohiro@users.noreply.github.com>
2024-04-22 20:15:23 +0900

    kadai_005再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,9 +2,12 @@
 let weight;
 weight = 68;
 
-// 変数を宣言し伸長を定義
+// 変数を宣言し身長を定義
 let height;
 height = 1.7;
 
-// BMI=体重/(身長*身長)として四則演算の結果を出力
-console.log(weight/(height*height));
+// 変数を宣言しBMI=体重/(身長*身長)として四則演算にて計算し値を定義
+let bmi;
+bmi = (weight/(height*height));
+
+console.log(bmi);
